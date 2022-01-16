7f94ceeac6812c34f15533dac1f03ae0f9c892e4
J. Claude <demo.claude15@gmail.com>
2022-01-16 14:24:26 +0200

    form validation modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,57 @@
-// const form = document.getElementById('form');
+const loginForm = document.getElementById('loginForm');
+const username = document.getElementById('usernam');
+const password = document.getElementById('pass');
+function checkLoginInputs() {
+  const usernameValue = username.value.trim();
+  const passwordValue = password.value.trim();
 
-// const email = document.getElementById('emeyili');
+  if (usernameValue === '') {
+    setErrorFor(username, "Username can't be empty");
+    username.focus();
+  } else {
+    setSuccessFor(username);
+  }
+  if (passwordValue === '') {
+    setErrorFor(password, "password can't be empty");
+    password.focus();
+  } else {
+    setSuccessFor(password);
+  }
+}
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; //  here the parennnt is .form-control
+  const small = formControl.querySelector('small');
+  // add error message inside small
+  small.innerText = message;
 
-// const password = document.getElementById('pass');
+  //error class  added
+  formControl.className = 'form-control error';
+}
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  // small.innerText = message;
+  formControl.className = 'form-control success';
+}
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkLoginInputs();
+});
 
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   checkInputDataForLogin();
-// });
-// function checkInputDataForLogin() {
-//   const emailData = email.value.trim();
-//   const passwordData = password.value.trim();
-
-//   if (emailData === '') {
-//     setErrorTo(email, 'Fill your email');
-//   } else {
-//     setSuccessTo(email);
-//   }
-// }
-// function setErrorTo(input, message) {
-//   const username = input.parentElement;
-//   const msg = username.querySelector('small');
-
-//   msg.innerText = message;
-
-//   username.className = 'username error';
-// }
 function loginFunc(e) {
   event.preventDefault();
-  var email = document.getElementById('email').value;
+  var username = document.getElementById('usernam').value;
   var password = document.getElementById('pwd').value;
+  var errorMsg = document.getElementById('errorMsg').value;
 
-  var user = localStorage.getItem('username');
+  var user = localStorage.getItem('signedUpUsers');
   var data = JSON.parse(user);
   console.log(data);
+
+  if (user == null) {
+    errorMsg.innerText = 'Wrong username';
+  } else if (username == data.username && password == data.password) {
+    errorMsg.innerHTML = 'logged in';
+  } else {
+    errorMsg.innerHTML = 'wrong password';
+  }
 }
